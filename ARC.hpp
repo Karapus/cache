480184c13152a80b88fcf91ac9782b53189a4238
Karapus <evgeny.nay@outlook.com>
2020-09-24 13:46:37 +0300

    forgotten files added

---
+++ ARC.hpp
@@ -0,0 +1,135 @@
+#pragma once
+#include <iostream>
+#include "LRU.hpp"
+
+namespace cache 
+{
+
+template <typename KeyT>
+struct void_page_t
+{
+	KeyT id;
+};
+
+template <typename T = page_t, typename KeyT = int, typename GetEl = T (*) (const KeyT&)>
+class ARC_cache_t : public cache_t<T, KeyT, GetEl>
+{
+	static void_page_t<KeyT> get_void_page(const KeyT& key)
+	{
+		return {key};
+	}
+	
+	std::size_t p_;
+
+	LRU_cache_t<T, KeyT, GetEl> t1_;
+	LRU_cache_t<T, KeyT, GetEl> t2_;
+
+	LRU_cache_t<void_page_t<KeyT>, KeyT, void_page_t<KeyT> (*) (const KeyT&)> b1_;
+	LRU_cache_t<void_page_t<KeyT>, KeyT, void_page_t<KeyT> (*) (const KeyT&)> b2_;
+	
+	void replace(bool in_b2 = false);
+
+	public:
+	bool lookup(const KeyT &key);
+
+	ARC_cache_t(std::size_t size, GetEl get_elem_ptr) :
+		cache_t<T, KeyT, GetEl>(size, get_elem_ptr),
+		p_(0),
+		t1_(size, get_elem_ptr),
+		t2_(size, get_elem_ptr),
+		b1_(size, &get_void_page),
+		b2_(size, &get_void_page)
+	{}
+
+	friend std::ostream & operator << (std::ostream &os, const ARC_cache_t &cache)
+	{
+		std::cout					\
+		       << "T1 = " << cache.t1_ << std::endl	\
+		       << "B1 = " << cache.b1_ << std::endl	\
+		       << "T2 = " << cache.t2_ << std::endl	\
+		       << "B2 = " << cache.b2_ << std::endl;
+		return os;
+	}
+};
+
+template <typename T, typename KeyT, typename GetEl>
+bool ARC_cache_t<T, KeyT, GetEl>::lookup(const KeyT &key)
+{
+#ifdef DEBUG
+	std::cout << "Key = " << key << std::endl	\
+		<< "p = " << p_ << std::endl		\
+		<< *this << std::endl;
+#endif
+	auto hit = t1_.find(key);
+	if (t1_.inside(hit))
+	{
+		move(t1_, t2_, hit);
+		return true;
+	}
+
+	hit = t2_.find(key);
+	if (t2_.inside(hit))
+	{
+		t2_.advance_to_mru(hit);
+		return true;
+	}
+
+	auto ghost_hit = b1_.find(key);
+	if (b1_.inside(ghost_hit))
+	{
+		p_ = std::min(t1_.sz_ + ((b1_.get_size() >= b2_.get_size()) ? 1 : b2_.get_size()/b1_.get_size()), cache_t<T, KeyT, GetEl>::sz_);
+		replace();
+		t2_.emplace_mru(key);
+		b1_.erase(ghost_hit);
+		return false;
+	}
+
+	ghost_hit = b2_.find(key);
+	if (b2_.inside(ghost_hit))
+	{
+		p_ = std::max((int)t1_.sz_ - (int)((b2_.get_size() >= b1_.get_size()) ? 1 : b1_.get_size()/b2_.get_size()), 0);
+		replace(true);
+		t2_.emplace_mru(key);
+		b2_.erase(ghost_hit);
+		return false;
+	}
+
+	if (t1_.get_size() + b1_.get_size() == cache_t<T, KeyT, GetEl>::sz_)
+	{
+		if (t1_.get_size() < cache_t<T, KeyT, GetEl>::sz_)
+		{
+			b1_.erase_lru();
+			replace();
+		}
+		else
+			t1_.erase_lru();
+	}
+	else 
+	{
+		std::size_t size = t1_.get_size() + t2_.get_size() + b1_.get_size() + b2_.get_size();
+		if (size >= cache_t<T, KeyT, GetEl>::sz_)
+		{
+			if (size == 2 * cache_t<T, KeyT, GetEl>::sz_)
+				b2_.erase_lru();
+			replace();
+		}
+	}
+	t1_.emplace_mru(key);
+	return false;
+}
+
+template <typename T, typename KeyT, typename GetEl>
+void ARC_cache_t<T, KeyT, GetEl>::replace(bool in_b2)
+{
+	if ((t1_.get_size() && t1_.get_size() > p_) || (in_b2 && t1_.get_size() == p_))
+	{
+		b1_.emplace_mru(t1_.get_lru_key());
+		t1_.erase_lru();
+	}
+	else
+	{
+		b2_.emplace_mru(t2_.get_lru_key());
+		t2_.erase_lru();
+	}
+}
+}
---
+++ LRU.hpp
@@ -0,0 +1,105 @@
+#pragma once
+#include <unordered_map>
+#include <list>
+#include <iostream>
+#include <cassert>
+#include "cache.h"
+
+namespace cache 
+{
+template <typename T = page_t, typename KeyT = int, typename GetEl = T (*) (const KeyT&)>
+class LRU_cache_t : public cache_t<T, KeyT, GetEl>
+{
+	std::list<T> cache_;
+	
+	using ListIt = typename std::list<T>::iterator;
+	std::unordered_map<KeyT, ListIt> map_;
+
+	using MapConstIt = typename std::unordered_map<KeyT, ListIt>::const_iterator;
+
+	public:
+	void emplace_mru(const KeyT &key)
+	{
+		cache_.push_front((*cache_t<T, KeyT, GetEl>::get_elem_ptr_)(key));
+		map_[key] = cache_.begin();
+	}
+	
+	void erase(const MapConstIt &hit)
+	{
+		cache_.erase(hit->second);
+		map_.erase(hit);
+	}
+	
+	void erase_lru()
+	{
+		map_.erase(cache_.back().id);
+		cache_.pop_back();
+	}
+
+	KeyT get_lru_key() const
+	{
+		return cache_.back().id;
+	}
+
+	std::size_t get_size() const
+	{
+#ifdef DEBUG
+		assert(map_.size() == cache_.size());
+#endif
+		return map_.size();
+	}
+
+	friend void move(LRU_cache_t &src, LRU_cache_t &dest, const MapConstIt &hit)
+	{
+		dest.cache_.splice(dest.cache_.begin(), src.cache_, hit->second);
+                dest.map_.insert(*hit);
+                src.map_.erase(hit);
+	}
+
+	void advance_to_mru(const MapConstIt &hit)
+	{
+		auto eltit = hit->second;
+		if (eltit != cache_.begin())
+			cache_.splice(cache_.begin(), cache_, eltit, std::next(eltit));
+	}
+
+	MapConstIt find(const KeyT& key) const
+	{
+		return map_.find(key);
+	}
+
+	bool inside(const MapConstIt &hit) const
+	{
+		return hit != map_.end();
+	}
+
+	LRU_cache_t(std::size_t size, GetEl get_elem_ptr) :
+		cache_t<T, KeyT, GetEl>(size, get_elem_ptr)
+	{}
+
+	bool lookup(const KeyT &key);
+
+	friend std::ostream & operator << (std::ostream &os, const LRU_cache_t &cache)
+	{
+		for (auto it = cache.cache_.begin(); it != cache.cache_.end(); it++) 
+			os << it->id << ' ';
+		return os;
+	}
+};
+
+template <typename T, typename KeyT, typename GetEl>
+bool LRU_cache_t<T, KeyT, GetEl>::lookup(const KeyT &key)
+{
+	auto hit = find(key);
+	if (!inside(hit))
+	{
+		if (get_size() == cache_t<T, KeyT, GetEl>::sz_)
+			erase_lru();
+		emplace_mru(key);
+		return false;
+	}
+
+	advance_to_mru(hit);
+	return true;
+}
+}
